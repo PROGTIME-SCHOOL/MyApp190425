bcb0802f633f7a17194c5508d3981f3919a66a89
Ilya <viosagmir@gmail.com>
2025-04-19 12:59:56 +0300

    AddedTitle

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main(string[] args)
     {
+        System.Console.WriteLine("Программа вычитания");
+        
         int a = int.Parse(Console.ReadLine());
         int b = int.Parse(Console.ReadLine());
 
